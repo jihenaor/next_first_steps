24e92ee14eb6606b063e7161d8901f25c1dfd304
jihenaor <jihenaor@gmail.com>
2023-12-14 20:52:24 -0500

    menu

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,9 @@
+
+export default function PricingPage() {
+    return (
+        <div>
+            <h1>Pricing Page</h1>
+            <p>This is the about page content.</p>
+        </div>
+    );
+}
